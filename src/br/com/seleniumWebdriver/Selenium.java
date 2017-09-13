9277ef1e86d65d6205988580865c772b23f8cead
Lucas de Lima <lucas.llf2608@gmail.com>
2017-09-13 17:54:10 -0300

    atualizando

--- src/br/com/seleniumWebdriver/Selenium.java
+++ src/br/com/seleniumWebdriver/Selenium.java
@@ -1,12 +1,20 @@
 package br.com.seleniumWebdriver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class Selenium {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("hello world!");
+	
 		
+		WebDriver paginaWeb = new FirefoxDriver();
+		
+		paginaWeb.get("https://www.mercadolivre.com.br/");
+		
+		
+
 	}
 	
 	
