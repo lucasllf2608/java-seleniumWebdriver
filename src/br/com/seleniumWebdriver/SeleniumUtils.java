a76292c23efc0820e6ac663a31e286ab59b85b8e
Lucas de Lima <lucas.llf2608@gmail.com>
2017-11-09 15:36:59 -0300

    Atualizando class utils

--- src/br/com/seleniumWebdriver/SeleniumUtils.java
+++ src/br/com/seleniumWebdriver/SeleniumUtils.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+
+
 public class SeleniumUtils {
 	
 	
 	public static void setElementoPorNome(String nome, String chaveElemento, WebDriver paginaWeb){
 		WebElement objetoForm = paginaWeb.findElement(By.name(nome));
 		objetoForm.sendKeys(chaveElemento);
-		System.out.println("Valor "+chaveElemento +" inserido com sucesso no elemento com o nome"+ nome );
+		System.out.println("Valor "+chaveElemento +" inserido com sucesso no elemento com o nome: "+ nome );
 	}
 	
 
@@ -18,16 +20,30 @@ public class SeleniumUtils {
 	public static void setElementoPorId(String id, String chaveElemento, WebDriver paginaWeb){
 		WebElement objetoForm = paginaWeb.findElement(By.id(id));
 		objetoForm.sendKeys(chaveElemento);
-		System.out.println("Valor "+chaveElemento +" inserido com sucesso no elemento com o id"+ id );
+		System.out.println("Valor "+chaveElemento +" inserido com sucesso no elemento com o id: "+ id );
 	}
 	
 	
 	public static void setElementoPorXpath(String xpath, String chaveElemento, WebDriver paginaWeb){
 		WebElement objetoForm = paginaWeb.findElement(By.xpath(xpath));
 		objetoForm.sendKeys(chaveElemento);
-		System.out.println("Valor "+chaveElemento +" inserido com sucesso no elemento com o xpath"+ xpath);
+		System.out.println("Valor "+chaveElemento +" inserido com sucesso no elemento com o xpath: "+ xpath);
 		
 	}
+	
+	
+	public static void eventoDeClickPorNome(String nome, WebDriver pagina){ 
+		pagina.findElement(By.name(nome)).click(); 
+	}
+	
+	public static void eventoDeClickPorId(String id, WebDriver pagina){
+		pagina.findElement(By.id(id)).click();
+	}
+	
+	public static void eventoDeClickPorXpath(String xpath, WebDriver pagina){
+		pagina.findElement(By.xpath(xpath)).click();
+	}
+	
 	
 	
 	
